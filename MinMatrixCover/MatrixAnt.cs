dc1a3963affc085b3fc30421d45a6e2b904c9de8
suilevap <suilevap@gmail.com>
2012-03-29 19:41:40 +0000

    Initial project version

---
+++ MinMatrixCover/MatrixAnt.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinMatrixCover
+{
+    public class MatrixAnt : MatrixPav
+    {
+        protected float[] _feramonsValues;
+        Random _rnd = new Random();
+
+
+
+        public MatrixAnt(int width, int height)
+            : base(width, height)
+        { 
+        
+        }
+        protected override int GetBestRow(out int column)
+        {
+            float kof = 0.98f;
+
+            int row = _rowIndexes
+                .Select(x => new { j = x, prior = 0.0 + 1 * _rnd.NextDouble() })
+                .AsParallel().OrderByDescending(x =>  
+                    (float)(
+                        //GetRowValue(x.j)* kof + ( _feramonsValues[x.j]*( 1 - kof)) * x.prior)
+                        GetRowValue(x.j)* (kof + (( 1 - kof)) * x.prior))
+
+                        )
+                .First().j;
+            //float kof = 0.5f;
+            ////float sum = _rowIndexes.Sum(j => (float)(Math.Pow(GetRowValue(j),kof)*Math.Pow(_feramonsValues[j], 1-kof)));
+            //float sum = _rowIndexes.Sum(j => (float)((GetRowValue(j)* kof) * (_feramonsValues[j]*( 1 - kof))));
+            
+            //float rndV = (float)_rnd.NextDouble() * sum;
+            //int row = _rowIndexes.First();
+            //foreach (int j in _rowIndexes)
+            //{
+            //    //rndV -= (float)(Math.Pow(GetRowValue(j), kof) * Math.Pow(_feramonsValues[j], 1 - kof));
+            //    rndV -= (float)((GetRowValue(j) * kof) * (_feramonsValues[j] * (1 - kof)));
+            //    if (rndV <= 0)
+            //    {
+            //        row = j;
+            //        break;
+            //    }
+            //}
+            _feramonsValues[row] -= GetRowValue(row);
+            column = 0;
+            return row;
+        }
+
+        public override List<KeyValuePair<int, int>> Solve()
+        {
+            List<KeyValuePair<int, int>> result = null;
+            List<KeyValuePair<int, int>> solution;
+            _feramonsValues = new float[_height];
+            GenColumnValues();
+            GenRowValues();
+            _rowIndexes.ForEach(j => _feramonsValues[j] = 0*GetRowValue(j));
+
+            for (int i = 0; i < 30; i++)
+            {
+                ClearIndexes();
+                solution = base.Solve();
+                if (result == null || result.Count > solution.Count)
+                {
+                    result = solution;
+                }
+               // Console.Write("{0} ", solution.Count);
+
+                //_rowIndexes.ForEach(j => _feramonsValues[j] *= 0.95f);
+
+            }
+            return result;
+        }
+    }
+}
---
+++ MinMatrixCover/MatrixPav2.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinMatrixCover
+{
+    public class MatrixPav2 : MatrixPav
+    {
+
+        protected class State
+        {
+            private List<int> _rows;
+            private float _value;
+
+            public float Value { get { return _value; } protected set {_value=value;} }
+            public List<int> Rows { get { return _rows; } }
+
+            public State()
+            {
+                _rows = new List<int>();
+                _value = 0;
+            }
+
+            public void AddRow(int row, float value)
+            {
+                _rows.Add(row);
+                _value += value;
+            }
+
+            public State Clone()
+            {
+                State result = new State();
+                result.Value = this.Value;
+                result.Rows.AddRange(this.Rows);
+                return result;
+            }
+        }
+
+        private List<State> states;
+
+         public MatrixPav2(int width, int height)
+            : base(width, height)
+        { 
+        
+        }
+
+        public override List<KeyValuePair<int, int>> Solve()
+        {
+            List<KeyValuePair<int, int>> result = new List<KeyValuePair<int, int>>();
+            GenColumnValues();
+            GenRowValues();
+
+            RemoveEmptyRows();
+            _columnIndexes = _columnIndexes.AsParallel()
+                .OrderBy(i => GetColumnValue(i))
+                .ToList();
+
+            states = new List<State>();
+            State state = new State();
+            states.Add(state);
+            Random rnd = new Random(15);
+            while (!Empty())
+            {
+                State state2 = state.Clone();
+                
+                int column;
+                int row = GetBestRow(out column);
+                
+                state.AddRow(row, GetRowValue(row));
+                states.Add(state);
+
+                row = GetBest2Row(out column);
+
+                state2.AddRow(row, GetRowValue(row));
+                states.Add(state2);
+
+                state = states.OrderByDescending(x => x.Value *(float)(0.75 + 0.25 * rnd.NextDouble())).First();
+                states.Remove(state);
+                LoadState(state);
+            }
+
+            result = state.Rows.Select(x => new KeyValuePair<int, int>(x, 0)).ToList();
+            //while (!Empty())
+            //{
+            //    int column;
+            //    int row = GetSyndromRow(out column);
+
+            //    KeyValuePair<int, int> syndromElement = new KeyValuePair<int, int>(row, column);
+            //    result.Add(syndromElement);
+            //}
+            return result;
+        }
+
+        protected int GetBest2Row(out int column)
+        {
+            int row = _rowIndexes.AsParallel()
+                .OrderByDescending(j => GetRowValue(j))
+                .Skip(1)
+                .First();
+            column = 0;
+            return row;
+        }
+
+
+        protected void LoadState(State state)
+        {
+            ClearIndexes();
+
+            GenColumnValues();
+            GenRowValues();
+            foreach (int row in state.Rows)
+            {
+                RemoveAllByRow(row);
+            }
+        }
+
+    }
+}
