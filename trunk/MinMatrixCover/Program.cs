9d1c0a711028f731b37769808fadb8085a8ea580
Tatsiana.Kazak89@gmail.com <Tatsiana.Kazak89@gmail.com@dd940fa5-4d14-7b64-6b6e-ec0d730f49bc>
2012-03-31 19:02:33 +0000

    Added OFV

--- trunk/MinMatrixCover/Program.cs
+++ trunk/MinMatrixCover/Program.cs
@@ -10,52 +10,11 @@ namespace MinMatrixCover
     {
         static void Main(string[] args)
         {
-            //Matrix m0 = new Matrix(10, 10);
-            //m0.GenerateMatrix(0.3f);
-            //Console.WriteLine(m0.ToString());
-            //Console.WriteLine(m0.Print(m0.Solve2(1)));
-            ////Console.WriteLine(m0.Check(m0.Solve()));
-
-            //Matrix m0p = new MatrixPav(10, 10);
-            //m0p.GenerateMatrix(0.3f);
-            //Console.WriteLine(m0p.ToString());
-            //Console.WriteLine(m0p.Print(m0p.Solve()));
-            ////Console.WriteLine(m0p.Check(m0p.Solve()));
-
-            //Matrix m0pav2 = new MatrixPav2(10, 10);
-            //m0pav2.GenerateMatrix(0.3f);
-            //Console.WriteLine(m0pav2.ToString());
-            //Console.WriteLine(m0pav2.Print(m0pav2.Solve()));
-
-
-            //Matrix m = new MatrixPav(500, 20000);
-            ////Matrix m = new Matrix(1000, 1000);
-
-            ////Console.WriteLine(m.ToString());
-            //Stopwatch timer = new Stopwatch();
-            //timer.Start();
-            //m.GenerateMatrix(0.03f);
-            //timer.Stop();
-            //Console.WriteLine("Time: {0}", timer.ElapsedMilliseconds);
-            //Solve(m);
-
-            //for (float d = 0; d < 1; d += 0.05f)
-            //{
-            //    Matrix m2 = new Matrix(600, 2000);
-            //    m2.GenerateMatrix(d);
-            //    Console.WriteLine("Density {0}:", d);
-            //    Solve(m2);
-            //}
-
-            //Console.WriteLine(m.ToString());
-
-            //Console.WriteLine(m.Print(m.Solve()));
-
             PerfTest();
             Console.ReadKey();
         }
 
-        static int Solve(Matrix m, int prevN = 0)
+        static float Solve(Matrix m, float prevN = 0)
         {
             Stopwatch timer = new Stopwatch();
             timer.Start();
@@ -75,18 +34,19 @@ namespace MinMatrixCover
                     Console.ForegroundColor = ConsoleColor.Red;
                 }
             Console.WriteLine("Solve Time ms: {0}", timer.ElapsedMilliseconds);
-            Console.WriteLine("Solution N= {0}; Check={1}", solution.Count(), m.Check(solution));
+            Console.WriteLine("Solution N= {0}; Check={1} OFV={2}", solution.Count(), m.Check(solution), m.GetOFV(solution));
+            
             //Console.WriteLine(m.ToString());
-            return solution.Count();
+            return m.GetOFV(solution);
 
             //Console.WriteLine(m.Print(m.Solve()));
         }
 
-        static int Solve2(Matrix m, int prevN = 0)
+        static float Solve2(Matrix m, float prevN = 0)
         {
             Stopwatch timer = new Stopwatch();
             timer.Start();
-            var solution = m.Solve2(30);
+            var solution = m.Solve2(100);
             timer.Stop();
             if (solution.Count < prevN)
             {
@@ -107,20 +67,21 @@ namespace MinMatrixCover
             }
 
             Console.WriteLine("Solve2 Time ms: {0}", timer.ElapsedMilliseconds);
-            Console.WriteLine("Solution N= {0}; Check={1}", solution.Count(), m.Check(solution));
+            Console.WriteLine("Solution N= {0}; Check={1} OFV={2}", solution.Count(), m.Check(solution), m.GetOFV(solution));
             //Console.WriteLine(m.ToString());
 
             //Console.WriteLine(m.Print(m.Solve()));
-            return solution.Count;
+            return m.GetOFV(solution);
         }
 
         static void PerfTest()
         {
-            int n;
+            float n;
             int w = 500;
-            int h = 2000;
-            float density = 0.02f;
-            for (int i = 0; i < 10; i++)
+            int h = 25;
+            float density = 0.20f;
+            string path = "scp41.txt";
+            for (int i = 0; i < 1; i++)
             {
                 Matrix m = new Matrix(w, h);
                 Matrix mPav = new MatrixPav(w, h);
@@ -129,10 +90,10 @@ namespace MinMatrixCover
                 Matrix mPav2 = new MatrixAnt(w, h);
                 //Matrix mPav2S2 = new MatrixAnt(500, 2000);
 
-                m.ReadFromFile("scp41.txt");
-                mPav.ReadFromFile("scp41.txt");
-                mPav2.ReadFromFile("scp41.txt");
-                mPavS2.ReadFromFile("scp41.txt");
+                m.ReadFromFile(path);
+                mPav.ReadFromFile(path);
+                mPav2.ReadFromFile(path);
+                mPavS2.ReadFromFile(path);
                 //m.GenerateMatrix(density, i);
                 //mPav.GenerateMatrix(density, i);
                 //mPavS2.GenerateMatrix(density, i);
