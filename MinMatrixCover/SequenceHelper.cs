7a86ca1afc25d108e1c1cde6f9a339b28326e342
suilevap <suilevap@gmail.com>
2012-05-04 21:13:38 +0000

    Added orderByMy method

---
+++ MinMatrixCover/SequenceHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinMatrixCover
+{
+    static class SequenceHelper
+    {
+
+        public static IEnumerable<T> OrderByMy<T>(this IEnumerable<T> data, Func<T, float> key1, Func<T, float> key2)
+        {
+            return data.GroupBy(key1).OrderBy(g => g.Key).SelectMany(gr => gr.OrderBy(key2));
+        }
+    }
+}
