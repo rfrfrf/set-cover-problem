8e71fb4b4a964c8ff50faedf0c35f120a76e7270
Tatsiana.Kazak89 <Tatsiana.Kazak89@gmail.com>
2012-10-19 17:00:11 +0000

    before refactoring

--- MinMatrixCover/Program.cs
+++ MinMatrixCover/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Diagnostics;
+using System.IO;
 
 namespace MinMatrixCover
 {
@@ -10,7 +11,8 @@ namespace MinMatrixCover
     {
         static void Main(string[] args)
         {
-            PerfTest2();
+            
+            PerfTest4();
             Console.ReadKey();
         }
 
@@ -47,7 +49,8 @@ namespace MinMatrixCover
             Stopwatch timer = new Stopwatch();
             timer.Start();
             int state = 0;
-            var solution = m.Solve2(10000, out state);
+            int iterCount=0;
+            var solution = m.Solve2(10000, out state, out iterCount);
             timer.Stop();
             if (solution.Count < prevN)
             {
@@ -75,28 +78,33 @@ namespace MinMatrixCover
             return m.GetOFV(solution);
         }
 
-        static void GetStatistics(Matrix m, int n, float density)
+        static string GetStatistics(Matrix m, int n, float density)
         {
             int state = 0;
+            int iterationCount = 0;
             int[] counter = new int[3];
+            int sum = 0;
+            Stopwatch stopwatch = new Stopwatch();
+            
             for (int i = 0; i < n; i++)
             {
                 m.GenerateMatrix(density, i);
-                Console.WriteLine("{0} iteration", i);
+                //Console.WriteLine("{0} iteration", i);
                 //Console.WriteLine(m.ToString());
-                m.Solve2(1000, out state);
-                if (state == 1)
-                    Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                stopwatch.Start();
+                m.Solve2(10000, out state, out iterationCount);
+                stopwatch.Stop();
+                // if (state == 1)
+                //    Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
                 counter[state]++;
+                sum += iterationCount;
             }
-            //int iter = 27;
-            //m.GenerateMatrix(density, iter);
 
-            //Console.WriteLine(m.ToString());
-            //Console.WriteLine("{0} iteration", iter);
-            //m.Solve2(1000, out state);
-            //counter[state]++;
-            Console.WriteLine("Density={0}%, Convergense {1}%, Resolvent duplicate {2}%, j={3}", density*100, counter[2] * 100.0 / n, counter[1] * 100.0 / n, m.Width*density);
+            Console.WriteLine("Density={0}%, Convergence {1}%, Resolvent duplicate {2}%, j={3}, iterationCount={4}, time = {5} ms", density * 100, counter[2] * 100.0 / n, counter[1] * 100.0 / n, m.Width * density, sum/n, stopwatch.ElapsedMilliseconds/n);
+            //Console.WriteLine("Density={0}%, Iteration {1}", density * 100, sum/n);
+            //Console.WriteLine("{0}; {1};", density * 100, sum / n);
+            return string.Format("{0}x{1};{2};{3};{4}", m.Width, m.Height, density * 100, sum / n, stopwatch.ElapsedMilliseconds / n);
+
         }
         static void PerfTest()
         {
@@ -104,7 +112,7 @@ namespace MinMatrixCover
             int w = 1000;
             int h = 200;
             float density = 0.20f;
-            string path = "scp41.txt";
+            string path = "scp51.txt";
             for (int i = 0; i < 1; i++)
             {
                 Matrix m = new Matrix(w, h);
@@ -183,17 +191,126 @@ namespace MinMatrixCover
         static void PerfTest2()
         {
             float n;
-            int w = 100;
-            int h = 40;
-            for (float i = 0.01f; i < 0.5f; i += 0.01f)
+            //int w = 20;
+            //int h = 15;
+            TextWriter resultFile = new StreamWriter("resultFile.csv");
+            resultFile.WriteLine("WxH; density; count; time(ms)");
+            float[] densities = new float[]{0.05f, 0.08f, 0.15f, 0.2f, 0.4f, 0.6f};
+            string[] sizes = new string[] { "20x40", "20x80", "30x100", "40x100", "50x120", "60x150", "80x200" }; //"100x300"
+
+           // string[] sizes = new string[] {"20x40", "40x100", "100x300", "400x800"};
+            foreach(var size in sizes)
             {
-
-                Matrix m = new Matrix(w, h);
-                GetStatistics(m, 100, i);
+                int w = int.Parse(size.Split('x')[0]);
+                int h = int.Parse(size.Split('x')[1]);
+                Console.WriteLine("Size: {0}", size);
+                foreach (var density in densities)
+                {
+                    Console.WriteLine("    Density: {0}", density);
+                    Matrix m = new Matrix(w, h);
+                    resultFile.WriteLine(GetStatistics(m, 1, density));
+                    resultFile.Flush();
+                }
             }
-           // Matrix m = new Matrix(w, h);
+            // Matrix m = new Matrix(w, h);
             //GetStatistics(m, 1000, 0.4f);
+            resultFile.Close();
+            Console.WriteLine("end");
+        }
 
+        static void PerfTest3()
+        {
+            float n;
+
+            //Matrix m = new Matrix(10, 10);
+            //m.GenerateMatrixForTest(0.2f, 4);
+            //Console.WriteLine(m.ToString());
+
+            //resultFile.WriteLine("WxH; density; count; time(ms)");
+            float[] densities = new float[] { 0.05f, 0.08f, 0.15f, 0.2f, 0.4f, 0.6f };
+            string[] sizes = new string[] { "20x40", "20x80", "30x100", "40x100", "50x120", "60x150", "80x200" }; //"100x300"
+
+            // string[] sizes = new string[] {"20x40", "40x100", "100x300", "400x800"};
+            foreach (var size in sizes)
+            {
+                int w = int.Parse(size.Split('x')[0]);
+                int h = int.Parse(size.Split('x')[1]);
+                Console.WriteLine("Size: {0}", size);
+                int state;
+                int iterCount;
+                foreach (var density in densities)
+                {
+                    Console.WriteLine("    Density: {0}", density);
+                    Matrix m = new Matrix(w, h);
+                    //m.GenerateMatrixForTest(density, h/20);
+                    m.GenerateMatrix(density);
+                    var t=m.Solve2(10000, out state, out iterCount);
+                    TextWriter resultFile = new StreamWriter(string.Format("resultDynamicFile_{0}_{1}.csv", size, density));
+                    foreach(var res in m.ResultDynamic){
+                    resultFile.WriteLine(string.Format("{0}; {1}", res.Key, res.Value));
+                    }
+                   // resultFile.WriteLine(GetStatistics(m, 1, density));
+                    resultFile.Flush();
+                    resultFile.Close();
+
+                }
+            }
+            // Matrix m = new Matrix(w, h);
+            //GetStatistics(m, 1000, 0.4f);
+            Console.WriteLine("end");
+        }
+
+        static void PerfTest4()
+        {
+            float n;
+
+            //Matrix m = new Matrix(10, 10);
+            //m.GenerateMatrixForTest(0.2f, 4);
+            //Console.WriteLine(m.ToString());
+
+            //resultFile.WriteLine("WxH; density; count; time(ms)");
+            float[] densities = new float[] { 0.05f };
+            string[] sizes = new string[] { "80x300" }; //"100x300"
+
+            // string[] sizes = new string[] {"20x40", "40x100", "100x300", "400x800"};
+            foreach (var size in sizes)
+            {
+                int w = int.Parse(size.Split('x')[0]);
+                int h = int.Parse(size.Split('x')[1]);
+                Console.WriteLine("Size: {0}", size);
+                int state;
+                int iterCount;
+                foreach (var density in densities)
+                {
+                    TextWriter resultFile = new StreamWriter(string.Format("resultDynamicFile_{0}_{1}.csv", size, density));
+                    for (int i = 1; i < 2; i++)
+                    {
+                        Console.WriteLine("    Density: {0}", density);
+                        Matrix m = new Matrix(w, h);
+                        //Matrix m = new MatrixPav(w, h);
+                        
+                        //m.GenerateMatrixForTest(density, h/20);
+                        //m.GenerateMatrix(density);
+                        m.GenerateMatrixForTest2(density, 15, i);
+                        var t = m.Solve2(1000000, out state, out iterCount);
+                        foreach (var res in m.ResultDynamic)
+                        {
+                            resultFile.WriteLine(string.Format("{0}; {1}", res.Key, res.Value));
+
+                        }
+                        resultFile.Flush();
+                        resultFile.WriteLine();
+                    }
+                    
+                    // resultFile.WriteLine(GetStatistics(m, 1, density));
+                    
+                    resultFile.Close();
+
+                }
+            }
+            // Matrix m = new Matrix(w, h);
+            //GetStatistics(m, 1000, 0.4f);
+            Console.WriteLine("end");
         }
     }
 }
